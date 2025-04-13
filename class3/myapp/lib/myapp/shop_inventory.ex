ac1843eaa5030d4a9752d1f0b78961502815974e
IwoSzczepniak <iszczepaniak@student.agh.edu.pl>
2025-04-13 23:13:54 +0200

    lab3 exercise 3

--- class3/myapp/lib/myapp/shop_inventory.ex
+++ class3/myapp/lib/myapp/shop_inventory.ex
@@ -3,41 +3,45 @@ defmodule MyApp.ShopInventory do
 
   # =====EXERCISE 2=====
   # Client API
-  def start_link(opts) do
-    GenServer.start_link(__MODULE__, opts)
-  end
+  # def start_link(opts) do
+  #   GenServer.start_link(__MODULE__, opts)
+  # end
 
-  def create_item(pid, item) do
-    GenServer.cast(pid, {:create_item, item})
-  end
+  # def create_item(pid, item) do
+  #   GenServer.cast(pid, {:create_item, item})
+  # end
 
-  def list_items(pid) do
-    GenServer.call(pid, :list_items)
-  end
+  # def list_items(pid) do
+  #   GenServer.call(pid, :list_items)
+  # end
 
-  def delete_item(pid, item) do
-    GenServer.cast(pid, {:delete_item, item})
-  end
+  # def delete_item(pid, item) do
+  #   GenServer.cast(pid, {:delete_item, item})
+  # end
 
-  def get_item_by_name(pid, name) do
-    GenServer.call(pid, {:get_item_by_name, name})
-  end
+  # def get_item_by_name(pid, name) do
+  #   GenServer.call(pid, {:get_item_by_name, name})
+  # end
 
   # =====EXERCISE 3=====
-  def create_item(_item) do
-    # Your code here
+  def start_link(items) do
+    GenServer.start_link(__MODULE__, items, name: __MODULE__)
+  end
+
+  def create_item(item) do
+    GenServer.cast(__MODULE__, {:create_item, item})
   end
 
   def list_items() do
-    # Your code here
+    GenServer.call(__MODULE__, :list_items)
   end
 
-  def delete_item(_item) do
-    # Your code here
+  def delete_item(item) do
+    GenServer.cast(__MODULE__, {:delete_item, item})
   end
 
-  def get_item_by_name(_name) do
-    # Your code here
+  def get_item_by_name(name) do
+    GenServer.call(__MODULE__, {:get_item_by_name, name})
   end
 
   # =====EXERCISE 1=====

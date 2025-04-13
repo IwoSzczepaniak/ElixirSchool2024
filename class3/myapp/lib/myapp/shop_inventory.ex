ff9e794acdebe33dd44a29ff49ae6ed343673bc0
IwoSzczepniak <iszczepaniak@student.agh.edu.pl>
2025-04-13 23:09:13 +0200

    lab3 exercises 1_2

--- class3/myapp/lib/myapp/shop_inventory.ex
+++ class3/myapp/lib/myapp/shop_inventory.ex
@@ -3,24 +3,24 @@ defmodule MyApp.ShopInventory do
 
   # =====EXERCISE 2=====
   # Client API
-  def start_link(_opts) do
-    GenServer.start_link(__MODULE__, [])
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, opts)
   end
 
-  def create_item(_pid, _item) do
-    # Your code here
+  def create_item(pid, item) do
+    GenServer.cast(pid, {:create_item, item})
   end
 
-  def list_items(_pid) do
-    # Your code here
+  def list_items(pid) do
+    GenServer.call(pid, :list_items)
   end
 
-  def delete_item(_pid, _item) do
-    # Your code here
+  def delete_item(pid, item) do
+    GenServer.cast(pid, {:delete_item, item})
   end
 
-  def get_item_by_name(_pid, _name) do
-    # Your code here
+  def get_item_by_name(pid, name) do
+    GenServer.call(pid, {:get_item_by_name, name})
   end
 
   # =====EXERCISE 3=====
@@ -43,26 +43,28 @@ defmodule MyApp.ShopInventory do
   # =====EXERCISE 1=====
   # Server API
   @impl true
-  def init(_) do
-    {:ok, []}
+  def init(items) do
+    {:ok, items}
   end
 
   @impl true
   def handle_call(:list_items, _, state) do
-    {:reply, :ok, state}
+    {:reply, state, state}
   end
 
-  def handle_call({:get_item_by_name, _name}, _, state) do
-    {:reply, :ok, state}
+  def handle_call({:get_item_by_name, name}, _, state) do
+    item = Enum.find(state, fn item -> item.name == name end)
+    {:reply, item, state}
   end
 
   @impl true
-  def handle_cast({:create_item, _item}, state) do
-    {:noreply, state}
+  def handle_cast({:create_item, item}, state) do
+    {:noreply, [item | state]}
   end
 
-  def handle_cast({:delete_item, _item}, state) do
-    {:noreply, state}
+  def handle_cast({:delete_item, item_to_delete}, state) do
+    updated_state = Enum.reject(state, fn item -> item == item_to_delete end)
+    {:noreply, updated_state}
   end
 
   # For supervisor testing
